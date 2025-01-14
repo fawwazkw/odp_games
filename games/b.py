950db374e059983f2a53d85b13c545bca059a51c
fawwazkw <fawwazkwidyadhana@gmail.com>
2025-01-14 16:25:35 +0700

    Update Fix Insyaallah

--- games/b.py
+++ games/b.py
@@ -1,4 +1,3 @@
-
 def b():
     """
     Diberikan sebuah list angka, cari pasangan angka dengan selisih terkecil.
@@ -10,17 +9,25 @@ def b():
     15 16
     """
 
-    # Meminta input dari pengguna
+
     user_input = input("Masukkan daftar angka yang dipisahkan oleh spasi: ")
     
-    # Mengubah input string menjadi daftar angka
+
     data = list(map(int, user_input.split()))
 
-    # Mengurutkan data
+
     data.sort()
 
-    # Mencari pasangan dengan selisih terkecil
-    min_diff_pair = min(zip(data, data[1:]), key=lambda x: x[1] - x[0])
+
+    min_diff = float('inf')
+    min_diff_pair = (None, None)
+
+
+    for i in range(len(data) - 1):
+        diff = data[i + 1] - data[i]
+        if diff < min_diff:
+            min_diff = diff
+            min_diff_pair = (data[i], data[i + 1])
 
     # Menampilkan pasangan dengan selisih terkecil
-    print("Pasangan angka dengan selisih terkecil:", *min_diff_pair)
+    print("Pasangan angka dengan selisih terkecil:", min_diff_pair[0], min_diff_pair[1])
